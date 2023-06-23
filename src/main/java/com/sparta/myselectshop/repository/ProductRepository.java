34d0396ae1f2b2c93fe4c87c6ca60fb44cdfa275
azuressu <martina0130@naver.com>
2023-06-23 15:46:05 +0900

    SQL쿼리문 작성해보기

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -16,4 +16,43 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // 앞에 User를 받아옴, ProductFolder에서 양방향 설정을 해줌
     // 받아온 folderId를 넣어주면 해당하는 프로덕트 종류를 가져올 수 있다 (특정 폴더에 속해있는 프로덕트 리스트가 !)
     // 해당 folder_id를 주면, 거기에 해당하는 product_id 리스트를 가져올 수 있다.
+
+    /* select *
+    from product p
+    where p.user_id = 1;
+
+    select p.id, p.title as product_title, pf.product_id as product_id, pf.folder_id as folder_id
+    from
+        product p left join product_folder pf
+            on p.id = pf.product_id
+    where p.user_id = 1;
+
+
+    select p.id, p.title as product_title, pf.product_id as product_id, pf.folder_id as folder_id
+    from
+        product p left join product_folder pf
+                            on p.id = pf.product_id
+    where p.user_id = 1
+        and pf.folder_id = 3;
+
+
+    select p.id, p.title as product_title, pf.product_id as product_id, pf.folder_id as folder_id
+    from
+        product p left join product_folder pf
+                            on p.id = pf.product_id
+    where p.user_id = 1
+        and pf.folder_id = 3
+    order by
+        p.id desc;
+
+    select p.id, p.title as product_title, pf.product_id as product_id, pf.folder_id as folder_id
+    from
+        product p left join product_folder pf
+                            on p.id = pf.product_id
+    where p.user_id = 1
+        and pf.folder_id = 3
+    order by
+        p.id desc
+        limit 10, 10; // 0부터 9까지가 10개, 10부터 19까지 10개,, 그러니까 앞에는 시작할 숫자, 뒤쪽에는 개수 !
+    */
 }
