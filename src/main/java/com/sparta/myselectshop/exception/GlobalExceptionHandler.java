d8501ffb2f1c4f45f9000d0e3cba9c72038a8609
azuressu <martina0130@naver.com>
2023-07-12 11:06:35 +0900

    Error 메시지 관리하기

--- src/main/java/com/sparta/myselectshop/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/myselectshop/exception/GlobalExceptionHandler.java
@@ -20,5 +20,29 @@ public class GlobalExceptionHandler {
         );
     }
 
+    @ExceptionHandler({NullPointerException.class})
+    public ResponseEntity<RestApiException> nullPointerExceptionHandler(NullPointerException ex) {
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+
+        return new ResponseEntity<>(
+            // Http Body
+            restApiException,
+            // Http Status code
+            HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler({ProductNotFoundException.class})
+    public ResponseEntity<RestApiException> notFoundProductExceptionHandler(ProductNotFoundException ex) {
+        // 여기서의 not_found.value()는 출력될 때 보이는 부분
+        RestApiException restApiException = new RestApiException(ex.getMessage(), HttpStatus.NOT_FOUND.value());
+        return new ResponseEntity<>(
+            // Http Body
+            restApiException,
+            // Http Status Code
+            HttpStatus.NOT_FOUND // 얘는 postman에서 위쪽에 status를 표시할 때 뜸(404 Not Found 라던가)
+        );
+    }
+
 
 }
